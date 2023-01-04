39deecd397bc8c0907c7196da7b28353ecaf8991
José Pedro Sousa Almeida <80987531+PedroAlmeida88@users.noreply.github.com>
2023-01-04 23:22:46 +0000

    Add heartbear

---
+++ utils.h
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+
+#define FIFO_SERVIDOR "SERVIDOR"
+#define FIFO_CLIENTE "CLIENTE%d" // usar scanf com o pid que vem do utilizador
+
+typedef struct item
+{
+    int id;
+    char nome[50];
+    char categoria[50];
+    int valAtual;
+    int valCompreJa;
+    int duracao;
+    char usernameVendedor[50];
+    char usernameLicitador[50];
+} Item;
+
+typedef struct Utilizador
+{
+    char nome[50];
+    char password[50];
+    pid_t pid;
+} User;
+
+typedef struct {
+    Item item;
+    User user;
+    pid_t pid;
+    int comando;
+}Comando;
+
+typedef struct resposta
+{
+    int num;//Verificacao do user
+    Item item;
+    int comando;
+    pid_t pid;
+} Resposta;
+
+
+int numArgumentos(char str[]);
+void modifyLineInFile(char *filename, int lineNumber, Item file);
+void deleteLineFromFile(char *filename, int lineNumber);
